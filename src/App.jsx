839f352ffa5c748e5f20a94f35af40c947b6b13c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-03 18:04:00 +0700

    feat: create routing and component page login and forgotpassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPasswordPage />,
+  },
+]);
+
 function App() {
-  return <div>Hello World</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
