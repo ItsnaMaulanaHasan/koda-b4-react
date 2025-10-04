5a298bce9676fdaef1012ed8f9e8caeef7b93164
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-04 23:29:29 +0700

    feat: create component card menu

---
+++ src/components/CardMenu.jsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+
+function CardMenu({ dataMenu }) {
+  return (
+    <div className="w-[280px]">
+      <div>
+        <img src={dataMenu.image} alt={dataMenu.name} />
+      </div>
+      <div className="py-5 px-4  w-12/13 mx-auto -mt-13 z-50 bg-white shadow-md relative flex flex-col gap-3">
+        <h1 className="font-medium text-2xl">{dataMenu.name}</h1>
+        <p className="text-[#4F5665] text-sm">{dataMenu.description}</p>
+        <span className="font-medium text-2xl text-[#FF8906]">
+          IDR {dataMenu.price.toLocaleString("id")}
+        </span>
+        <div className="flex gap-2">
+          <Button className="bg-[#FF8906] flex-2">Buy</Button>
+          <Button className="flex-1 border border-[#FF8906] text-[#FF8906] justify-items-center content-center">
+            <img
+              src="/public/icon/icon-cart-orange.svg"
+              alt="Icon Cart Orange"
+            />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardMenu;
