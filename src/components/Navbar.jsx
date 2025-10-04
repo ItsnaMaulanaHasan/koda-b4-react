2d5ccb8ef6fb38ed5cf8f574b4843866bd7ba247
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-04 23:29:49 +0700

    fix: add z-index on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   const getNavLinkClass = ({ isActive }) => `hover:font-bold py-1 transition duration-300 ${isActive && "font-bold border-b border-[#FF8906]"}`;
   return (
     <header>
-      <nav className="flex w-full fixed top-0 justify-between items-center px-4 sm:px-10 md:px-20 lg:px-40 py-5 bg-[#0B090921]">
+      <nav className="flex w-full fixed top-0 justify-between items-center px-4 sm:px-10 md:px-20 lg:px-40 z-100 py-5 bg-[#0B090921]">
         <div className="flex items-center gap-5 sm:gap-7 md:gap-10">
           <div>
             <img src="/public/icon/logo-white.svg" alt="Logo white" />
@@ -25,15 +25,29 @@ function Navbar() {
         </div>
         <div className="flex gap-5 items-center">
           <button className="h-6 w-6 flex-shrink-0 cursor-pointer">
-            <img className="h-full w-full" src="/public/icon/icon-search.svg" alt="Icon Search" />
+            <img
+              className="h-full w-full"
+              src="/public/icon/icon-search.svg"
+              alt="Icon Search"
+            />
           </button>
           <button className="h-6 w-6 flex-shrink-0 cursor-pointer">
-            <img className="h-full w-full" src="/public/icon/icon-cart.svg" alt="Icon Cart" />
+            <img
+              className="h-full w-full"
+              src="/public/icon/icon-cart.svg"
+              alt="Icon Cart"
+            />
           </button>
-          <Button onCLick={() => navigate("/login")} className="border border-white px-4 text-white whitespace-nowrap">
+          <Button
+            onCLick={() => navigate("/login")}
+            className="border border-white px-4 text-white whitespace-nowrap"
+          >
             Sign In
           </Button>
-          <Button onCLick={() => navigate("/register")} className="px-4 bg-[#FF8906] whitespace-nowrap">
+          <Button
+            onCLick={() => navigate("/register")}
+            className="px-4 bg-[#FF8906] whitespace-nowrap"
+          >
             Sign Up
           </Button>
         </div>
