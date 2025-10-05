f2c9043c070a7764fc6a70142ddca9075c58f3d5
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-05 08:57:48 +0700

    feat: create component footer and add to outlet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,61 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="grid grid-cols-4 gap-5 bg-[#F8F8F8] px-30 py-20">
+        <div className="flex flex-col gap-5 justify-end">
+          <div>
+            <img src="/public/icon/logo-original.svg" alt="" />
+          </div>
+          <p className="text-[#4F5665]">
+            Coffee Shop is a store that sells some good meals, and especially
+            coffee. We provide high quality beans
+          </p>
+          <span className="text-[#AFB5C0] text-sm">©2020CoffeeStore</span>
+        </div>
+        <div>
+          <h1 className="mb-5">Product</h1>
+          <ul className="flex flex-col gap-4 text-[#4F5665]">
+            <li>Our Product</li>
+            <li>Pricing</li>
+            <li>Locations</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mb-5">Engage</h1>
+          <ul className="flex flex-col gap-4 text-[#4F5665]">
+            <li>Our Product</li>
+            <li>Pricing</li>
+            <li>Locations</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mb-5">Social Media</h1>
+          <ul className="flex items-center justify-start gap-5">
+            <li className="bg-[#0B132A] rounded-full">
+              <a href="https://www.facebook.com">
+                <img src="/public/icon/icon-facebook.svg" alt="Icon Facebook" />
+              </a>
+            </li>
+            <li className="bg-[#0B132A] rounded-full">
+              <a href="https://www.x.com">
+                <img src="/public/icon/icon-twitter.svg" alt="Icon Twitter" />
+              </a>
+            </li>
+            <li className="bg-[#0B132A] rounded-full">
+              <a href="https://www.instagram.com">
+                <img
+                  src="/public/icon/icon-instagram.svg"
+                  alt="Icon Instagram"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
