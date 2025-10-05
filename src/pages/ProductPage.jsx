8147d36d85147ac6c17b7a2a57e6f0e0b1e5e6ba
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-06 02:01:16 +0700

    fix: change path image bg product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -82,7 +82,7 @@ function ProductPage() {
   return (
     <>
       <div className="flex flex-col gap-10 mt-20 mb-20">
-        <div className="px-20 py-20 bg-[url('img/img-header-product.png')] font-medium text-white text-5xl bg-no-repeat bg-cover bg-center">
+        <div className="px-20 py-20 bg-[url('/img/img-header-product.png')] font-medium text-white text-5xl bg-no-repeat bg-cover bg-center">
           We Provide Good Coffee and Healthy Meals
         </div>
         <div>
