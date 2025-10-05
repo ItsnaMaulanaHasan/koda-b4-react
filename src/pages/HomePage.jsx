6881a2de2c3cbce1215dab527cb730cf3134aafb
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-05 08:58:29 +0700

    feat: update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
 import Button from "../components/Button";
+import CardMenu from "../components/CardMenu";
+import Testimoni from "../components/Testimoni";
+import { useFetchData } from "../hooks/useFetchData";
 
 function HomePage() {
+  const { data, isLoading, error } = useFetchData("/public/data/menu.json");
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error: {error}</div>;
   return (
     <main>
+      {/* section 1 */}
       <div className="flex">
         {/* sebelah kiri */}
         <div className="flex-1 flex flex-col">
@@ -45,7 +54,7 @@ function HomePage() {
           {/* konten bawah */}
           <div className="flex-1 flex flex-col gap-5 px-20 justify-center">
             <div className="flex items-center">
-              <div className="border-l-6 border-[#FF8906] w-1 h-15"></div>
+              <div className="bg-[#FF8906] w-2 h-15"></div>
               <div className="font-medium text-5xl p-5">
                 We Provide <span className="text-[#8E6447]">Good Coffee</span>{" "}
                 and <span className="text-[#8E6447]">Healthy Meals</span>
@@ -106,6 +115,45 @@ function HomePage() {
             />
           </div>
         </div>
+      </div>
+      {/* section 2 */}
+      <div className="flex flex-col gap-10 mt-20 px-20">
+        <div className="flex flex-col gap-5 items-center">
+          <h1 className="font-medium text-5xl">
+            Here is People's <span className="text-[#8E6447]">Favorite</span>
+          </h1>
+          <div className="h-2 w-20 bg-[#FF8906]"></div>
+          <p className="text-[#4F5665]">
+            Let's choose and have a bit taste of poeple's favorite. It might be
+            yours too!
+          </p>
+        </div>
+        <div className="grid grid-cols-4">
+          {data.slice(0, 4).map((menu) => (
+            <CardMenu key={menu.id} dataMenu={menu} />
+          ))}
+        </div>
+      </div>
+      {/* section 3 */}
+      <div className="flex flex-col gap-10 mt-20 bg-[#E8E8E84D] py-10 px-20">
+        <div className="flex flex-col gap-5 items-center">
+          <h1 className="font-medium text-5xl">
+            <span className="text-[#8E6447]">Visit Our Store</span> in the Spot
+            on the Map Below
+          </h1>
+          <div className="h-2 w-20 bg-[#FF8906]"></div>
+          <p className="text-[#4F5665]">
+            Let's choose and have a bit taste of poeple's favorite. It might be
+            yours too!
+          </p>
+        </div>
+        <div className="mt-20">
+          <img src="/public/icon/map.svg" alt="Map Store" />
+        </div>
+      </div>
+      {/* section 4 */}
+      <div className="bg-custom-gradient px-20 py-10 mb-20">
+        <Testimoni />
       </div>
     </main>
   );
