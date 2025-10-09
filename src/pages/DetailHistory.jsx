7a9a2d86b982cf14e94c29cadadfdefd44edab6a
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-09 16:26:29 +0700

    feat: set structure detail history

--- src/pages/DetailHistory.jsx
+++ src/pages/DetailHistory.jsx
@@ -1,8 +1,73 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getOrderHistories } from "../utils/orderUtils";
+import moment from "moment";
+import CardOrder from "../components/CardOrder";
 
 function DetailHistory() {
   const { noOrder } = useParams();
-  return <div className="mt-20">{noOrder}</div>;
+  const [history, setHistory] = useState(null);
+
+  useEffect(() => {
+    const data = getOrderHistories().find(
+      (history) => history.noOrder === noOrder
+    );
+    setHistory(data);
+  }, [noOrder]);
+
+  if (!history) return <div className="mt-20 p-20">Loading...</div>;
+
+  return (
+    <div className="mt-20 p-20">
+      <h1 className="font-medium text-4xl mb-5">
+        Order <span className="font-bold">{history.noOrder}</span>
+      </h1>
+      <p className="text-sm text-[#4F5665]">
+        {moment(history.dateOrder).format("DD MMMM YYYY [at] hh:mm A")}
+      </p>
+      <div className="grid grid-cols-2 mt-10 gap-5">
+        <div>
+          <h1 className="text-2xl font-medium">Order Information</h1>
+          <div className="flex flex-col">
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.fullName}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.address}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.phone}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.paymentMethod}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.shipping}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.status}</div>
+            </div>
+            <div className="px-3 py-5 flex justify-between items-center">
+              <div>1</div>
+              <div>{history.totalTransaction}</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl font-medium">Your Order</h1>
+          {history.listOrders.map((item) => (
+            <CardOrder key={item.cartId} order={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DetailHistory;
