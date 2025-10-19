04671a859d3df848a1c91db355a69b78200e7c51
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-19 10:03:18 +0700

    feat: adjust footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /**
  * Footer component displaying company information, navigation links, and social media
  * @returns {JSX.Element} Footer component with company branding, product links, engagement links, and social media icons
@@ -17,26 +19,88 @@ function Footer() {
         </div>
         <div className="flex justify-between gap-20">
           <div>
-            <h1 className="mb-5">Product</h1>
+            <h1 className="mb-5 font-semibold text-[#0B132A]">Product</h1>
             <ul className="flex flex-col gap-4 text-[#4F5665]">
-              <li>Our Product</li>
-              <li>Pricing</li>
-              <li>Locations</li>
-              <li>Blog</li>
+              <li>
+                <Link
+                  to="/product"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Our Product
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/pricing"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/locations"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Locations
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/countries"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Countries
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blog"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Blog
+                </Link>
+              </li>
             </ul>
           </div>
           <div>
-            <h1 className="mb-5">Engage</h1>
+            <h1 className="mb-5 font-semibold text-[#0B132A]">Engage</h1>
             <ul className="flex flex-col gap-4 text-[#4F5665]">
-              <li>Our Product</li>
-              <li>Pricing</li>
-              <li>Locations</li>
-              <li>Blog</li>
+              <li>
+                <Link
+                  to="/partner"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Partner
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/faq"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/privacy-policy"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/terms"
+                  className="hover:text-[#FF8906] hover:font-medium transition-all duration-300">
+                  Terms of Service
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
         <div>
-          <h1 className="mb-5">Social Media</h1>
+          <h1 className="mb-5 font-semibold text-[#0B132A]">Social Media</h1>
           <ul className="flex items-center justify-start gap-5">
             <li className="bg-[#0B132A] rounded-full">
               <a href="https://www.facebook.com">
