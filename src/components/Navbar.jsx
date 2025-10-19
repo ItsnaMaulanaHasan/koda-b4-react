fd0e1bf73278275333815871d649737b779554d4
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-19 10:53:23 +0700

    feat: create badge amoun cart on icon vart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { useSelector } from "react-redux";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { DrawerNavbarContext } from "../context/DrawerContext";
@@ -20,6 +21,8 @@ function Navbar() {
   const [showDropdown, setShowDropdown] = useState(false);
   const [showSearchDesktop, setShowSearchDesktop] = useState(false);
   const { userLogin, setUserLogin } = useContext(AuthContext);
+  const dataCarts = useSelector((state) => state.cart.dataCarts);
+  const amountCart = dataCarts.length;
 
   const isHomePage = location.pathname === "/";
   const isAdminPage = location.pathname.startsWith("/admin");
@@ -113,7 +116,7 @@ function Navbar() {
             </div>
             <button
               onClick={() => navigate("/cart")}
-              className="flex-shrink-0 w-5 h-5 cursor-pointer lg:h-6 lg:w-6">
+              className="relative flex-shrink-0 w-5 h-5 cursor-pointer lg:h-6 lg:w-6">
               <img
                 className="w-full h-full"
                 src={
@@ -123,6 +126,11 @@ function Navbar() {
                 }
                 alt="Icon Cart"
               />
+              {amountCart > 0 && (
+                <div className="absolute flex items-center justify-center h-5 px-1 text-xs font-semibold text-white bg-red-500 rounded-full -top-2 -right-2 min-w-5">
+                  {amountCart}
+                </div>
+              )}
             </button>
 
             {userLogin ? (
