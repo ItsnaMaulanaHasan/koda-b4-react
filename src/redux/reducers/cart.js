3fe0e7576abc3b20e893a3100a25d31c2eac5a8b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-22 15:25:24 +0700

    feat: adjust reducer cart

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,42 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  dataCarts: [],
+  amountCarts: 0,
 };
 
 const cart = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addDataCart: (state, action) => {
-      const existingItemIndex = state.dataCarts.findIndex(
-        (cartItem) =>
-          cartItem.menuId === action.payload.menuId &&
-          cartItem.size === action.payload.size &&
-          cartItem.hotIce === action.payload.hotIce
-      );
-
-      if (existingItemIndex > -1) {
-        state.dataCarts[existingItemIndex].quantity += action.payload.quantity;
-      } else {
-        state.dataCarts.push({
-          ...action.payload,
-          cartId: Date.now(),
-        });
-      }
+    setAmountCarts: (state, action) => {
+      state.amountCarts = action.payload;
     },
-    removeDataCart: (state, action) => {
-      state.dataCarts = state.dataCarts.filter(
-        (cart) => cart.cartId !== action.payload
-      );
+    addAmountCarts: (state) => {
+      state.amountCarts++;
     },
-    clearDataCart: (state) => {
-      state.dataCarts = [];
+    reduceAmountCarts: (state) => {
+      state.amountCarts--;
     },
   },
 });
 
 export const {
   reducer: cartReducer,
-  actions: { addDataCart, removeDataCart, clearDataCart },
+  actions: { setAmountCarts, addAmountCarts, reduceAmountCarts },
 } = cart;
